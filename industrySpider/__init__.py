edb6c404b214f9094001a2f8be80bb86be6135f2
wangerge <wangerge@github.com>
2018-10-13 22:18:41 +0800

    System

---
+++ industrySpider/__init__.py
@@ -0,0 +1,12 @@
+#!~/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@version  : python 3.6
+@author   : kangrl
+@contact  : kangrl@live.cn
+@software : PyCharm 
+@filename : __init__.py 
+@Creattime: 2018/8/28 15:42
+"""
+
+# from industrySpider.BaseSpider import *
