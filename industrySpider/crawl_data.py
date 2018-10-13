e3f31ba4b42b1cb8b9acc689f0c7375d66c8c899
wangerge <wangerge@github.com>
2018-10-13 22:35:41 +0800

    运行爬虫

---
+++ industrySpider/crawl_data.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+from industrySpider import \
+    civilcn,\
+    hiyd,\
+    amdaily,\
+    bio360,\
+    ccid,\
+    ce,\
+    chinacwa,\
+    DoNews,\
+    chemnet,\
+    healthcare,\
+    lawlib,\
+    xaedu,\
+    leiphone,\
+    whcm,\
+    zhongfang,\
+    ziguan,\
+    xazbzjy
+
+try:
+    civilcn.civilcn_run(1)
+    print('civilcn爬取完成')
+except:
+    print('civilcn爬取错误')
+    pass
+
+try:
+    hiyd.hiyd_run(1)
+    print('hiyd爬取完成')
+except:
+    print('hiyd爬取错误')
+    pass
+
+try:
+    amdaily.amdaily_run(1)
+    print('amdaily爬取完成')
+except:
+    print('amdaily爬取错误')
+    pass
+
+
+try:
+    bio360.bio360_run(1)
+    print('bio360爬取完成')
+except:
+    print('bio360爬取错误')
+    pass
+
+try:
+    ccid.ccid_run(1)
+    print('ccid爬取完成')
+except:
+    print('ccid爬取错误')
+    pass
+
+try:
+    ce.ce_run()
+    print('ce爬取完成')
+except:
+    print('ce爬取错误')
+    pass
+
+try:
+    chinacwa.chinacwa_run(1)
+    print('chinacwa爬取完成')
+except:
+    print('chinacwa爬取错误')
+    pass
+
+try:
+    DoNews.DoNews_run(1)
+    print('DoNews爬取完成')
+except:
+    print('DoNews爬取错误')
+    pass
+
+try:
+    chemnet.chemnet_run(1)
+    print('chemnet爬取完成')
+except:
+    print('chemnet爬取错误')
+    pass
+
+try:
+    healthcare.healthcare_run(1)
+    print('lawlib爬取完成')
+except:
+    print('lawlib爬取错误')
+    pass
+
+try:
+    lawlib.lawlib_run(1)
+    print('lawlib爬取完成')
+except:
+    print('lawlib爬取错误')
+    pass
+
+try:
+    xaedu.xaedu_run(1)
+    print('xaedu爬取完成')
+except:
+    print('xaedu爬取错误')
+    pass
+
+try:
+    leiphone.leiphone_run(1)
+    print('leiphone爬取完成')
+except:
+    print('leiphone爬取错误')
+    pass
+
+try:
+    whcm.whcm_run(1)
+    print('whcm爬取完成')
+except:
+    print('whcm爬取错误')
+    pass
+
+try:
+    ziguan.ziguan_run(1)
+    print('ziguan爬取完成')
+except:
+    print('ziguan爬取错误')
+    pass
+
+try:
+    zhongfang.zhongfang_run(1)
+    print('zhongfang爬取完成')
+except:
+    print('zhongfang爬取错误')
+    pass
+
+try:
+    xazbzjy.xazbzjy_run(1)
+    print('xazbzjy爬取完成')
+except:
+    print('xazbzjy爬取错误')
+    pass
+
